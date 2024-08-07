afbbbfb70ffa78987d80b69f1ba6b0337d930aaa
quocan142536@gmail.com <quocan142536@gmail.com>
2024-08-07 13:33:48 +0700

    update

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,5 +1,7 @@
 import { io } from 'socket.io-client';
 
-const socket = io(`${process.env.NEXT_PUBLIC_API_URL}`);
+const socket = io(`${process.env.NEXT_PUBLIC_API_URL}`, {
+    withCredentials: true,
+});
 
 export default socket;
